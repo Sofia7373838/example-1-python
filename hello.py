f3232755fc9d498424d764da8cb441282db6339c
Sofia7373838 <sofiavanessanf@gmail.com>
2026-08-26 10:21:00 -0600

    reto

--- hello.py
+++ hello.py
@@ -1,3 +1,13 @@
+import hashlib
+import random
+
+
+def random_hash():
+    """Generate a random SHA-256 hash"""
+    random_value = str(random.random())
+    return hashlib.sha256(random_value.encode()).hexdigest()
+
+
 def add(x, y):
     """This is an add function"""
     return x + y
@@ -5,3 +15,4 @@ def add(x, y):
 
 if __name__ == "__main__":
     print(add(1, 1))
+    print(random_hash())
